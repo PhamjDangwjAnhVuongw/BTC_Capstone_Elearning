8436ecef2252aac859ebee0cca3868967fc85ddf
PhamjDangwjAnhVuongw <phamvuong8199@gmail.com>
2024-04-18 22:32:12 +0700

    .

--- src/components/CoursesList/CoursesList.jsx
+++ src/components/CoursesList/CoursesList.jsx
@@ -47,7 +47,7 @@ export default function CoursesList() {
           Khám phá khoá học
         </h1>
       </div>
-      <div className="courses__list grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 gap-7">
+      <div className="courses__list grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-7">
         {renderCoursesList}
       </div>
     </div>
